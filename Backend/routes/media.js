eaa3f5efb016eca02c9a684620a3b932b2a01a5c
Lobbbey <joshuatylerbandy@gmail.com>
2025-08-01 17:02:31 -0400

    Forgot to branch for media APIs

--- Backend/routes/media.js
+++ Backend/routes/media.js
@@ -1,9 +1,61 @@
 const express = require('express');
 const router = express.Router();
 
-// Example route
+// Test Route
 router.get('/', (req, res) => {
   res.json({ message: 'Media route working' });
+});
+
+// @route POST /api/media/add
+// @desc Adds media to a user
+// @in 
+// @out Result Message
+router.post('/add', (req, res) => {
+  res.json({message: "addmedia"});
+  try{
+
+  } catch(err){
+    console.error("Error:", err);
+    res.status(500).json({Result: error.message});
+  }
+});
+
+// @route POST /api/media/add
+// @desc Adds media to a user
+// @in 
+// @out Result Message
+router.post('/delete', (req, res) => {
+  res.json({message: "delete media"});
+  try{
+
+  } catch(err){
+    console.error("Error:", err);
+    res.status(500).json({Result: error.message});
+  }
+});
+
+// @route POST /api/media/add
+// @desc Adds media to a user
+// @in 
+// @out Result Message
+router.post('/search', (req, res) =>{
+  res.json({message: "search media"})
+  try{
+
+  } catch(err){
+    console.error("Error:", err);
+    res.status(500).json({Result: error.message});
+  }
+});
+
+router.post('/update', (req, res) => {
+  res.json({message: "updateMedia"});
+  try{
+
+  } catch(err){
+    console.error("Error:", err);
+    res.status(500).json({Result: error.message});
+  }
 });
 
 module.exports = router;
